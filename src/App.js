e0d9e75a0f0b32924b539c8b8894fa7986d3f661
Abhipatil11 <abhishekwarude15174@gmail.com>
2024-05-31 19:15:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
-import './App.css';
+import "../src/dist/styles.css";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Navbar from "../src/components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Models from "./Pages/Models";
+import TestimonialsPage from "./Pages/TestimonialsPage";
+import Team from "./Pages/Team";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Car Rental</h1>
-       
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="models" element={<Models />} />
+        <Route path="testimonials" element={<TestimonialsPage />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
